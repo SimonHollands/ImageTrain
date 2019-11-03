bbc51d95cd8d3e937e072d5d2db515ac59db5464
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-11-02 18:58:38 -0700

    update

--- train.py
+++ train.py
@@ -4,6 +4,6 @@ trainer=DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer = DetectionModelTrainer()
 trainer.setDataDirectory(data_directory="headsets2")
-trainer.setTrainConfig(object_names_array=["surfer"], batch_size=4, num_expexriments=200, 
+trainer.setTrainConfig(object_names_array=["surfer"], batch_size=4, num_experiments=5, 
 train_from_pretrained_model="/Users/hollands/dev/cfmodelserver/models/yolo-tiny.h5")
 trainer.trainModel()
