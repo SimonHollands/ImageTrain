4ea25bdc90c4518a45c6742465f88001a4472905
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-11-02 18:00:42 -0700

    Requiremts

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
-trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
+trainer = DetectionModelTrainer()
 trainer.setDataDirectory(data_directory="headsets2")
-trainer.setTrainConfig(object_names_array=["surfer"], batch_size=4, num_experiments=200, 
+trainer.setTrainConfig(object_names_array=["surfer"], batch_size=4, num_expexriments=200, 
 train_from_pretrained_model="/Users/hollands/dev/cfmodelserver/models/yolo-tiny.h5")
 trainer.trainModel()
